a157810b01b7743eebb634e02bb4d547fbad8de1
TanpaNama <nztmscreempendi@yahoo.co.id>
2023-07-24 21:56:23 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #/bin/bash
 #############################
 #      REQUIRED SETUP
-KSU=ndef # set to 1 to enable KernelSU; if not leave the same
+KSU=1 # set to 1 to enable KernelSU; if not leave the same
 
 INMODULES=ndef # set to 1 to include modules in updater zip 
 
-DEFCONFIG=ndef # set preferred existing defconfig in arch/arm64/configs
+DEFCONFIG=qgki_defconfig # set preferred existing defconfig in arch/arm64/configs
                # or if arch/arm64/configs does not contain it, specify 
                # a defconfig in THE SAME DIRECTORY WITH build.sh
                
-KERNEL_SOURCE=ndef # set to a preferred remote URL (e.g https://github.com/torvalds/linux...)
+KERNEL_SOURCE=https://github.com/MiCode/Xiaomi_Kernel_OpenSource # set to a preferred remote URL (e.g https://github.com/torvalds/linux...)
 
-KBRANCH="" # if not changed, use default kernel branch
+KBRANCH="-b veux-r-oss" # if not changed, use default kernel branch
            # set to "-b <kernel branch name>" if you want to
 CLANGDL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr2-release/clang-r383902b1.tar.gz"
         # direct link to Clang toolchain
