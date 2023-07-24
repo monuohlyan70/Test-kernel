b6934389cb1369175bd722b5233db03fbadf2cf3
RedEnemy30 <nztmscreempendi@yahoo.co.id>
2023-07-24 12:41:45 +0000

    main-old

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,37 +1,32 @@
-### AnyKernel3 Ramdisk Mod Script
-## osm0sis @ xda-developers
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
 
-### AnyKernel setup
+## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=veux_unknownversion
+kernel.string=veux
 do.devicecheck=1
-do.modules=1
+do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=1
 device.name1=veux
 device.name2=peux
 device.name3=veux_p
+supported.versions=
 '; } # end properties
 
-### AnyKernel install
-# begin attributes
-attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-} # end attributes
-
-
-## boot shell variables
+# shell variables
 block=/dev/block/by-name/boot;
 is_slot_device=1;
 ramdisk_compression=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh && attributes;
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
 
-# boot install
-dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-## end boot install
+## AnyKernel install
+dump_boot;
+
+write_boot;
+## end install
--- build.sh
+++ build.sh
@@ -1,21 +1,7 @@
 #/bin/bash
-#############################
-#      REQUIRED SETUP
-KSU=ndef # set to 1 to enable KernelSU; if not leave the same
 
-INMODULES=ndef # set to 1 to include modules in updater zip 
-
-DEFCONFIG=ndef # set preferred existing defconfig in arch/arm64/configs
-               # or if arch/arm64/configs does not contain it, specify 
-               # a defconfig in THE SAME DIRECTORY WITH build.sh
-               
-KERNEL_SOURCE=ndef # set to a preferred remote URL (e.g https://github.com/torvalds/linux...)
-
-KBRANCH="" # if not changed, use default kernel branch
-           # set to "-b <kernel branch name>" if you want to
-CLANGDL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr2-release/clang-r383902b1.tar.gz"
-        # direct link to Clang toolchain
-#############################
+#Uncomment for a KernelSU build
+#KSU=1
 
 case $HOSTNAME in
   (fv-az*)  ISACTIONS=1 ;;
@@ -24,38 +10,32 @@ esac
 
 getsource () {
     if [ ! -d "common" ]; then
-    echo ============================
     echo Downloading kernel source...
-    set -x
-    git clone --depth=1 $KERNEL_SOURCE $KBRANCH common
-    set +x
+    git clone --depth=1 https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b veux-r-oss common
     fi
 }
 gettools () {
-    set -x
     if [ ! -d "build" ]; then
     echo ==================================
     echo Downloading back-end build scripts
     git clone --depth=1 https://android.googlesource.com/kernel/build -b master-kernel-build-2021 build
-    fi
-    if [ ! -d "prebuilts" ]; then
+    elif [ ! -d "prebuilts" ]; then
     echo ============================
     echo Downloading build essentials
     git clone --depth=1 https://android.googlesource.com/kernel/prebuilts/build-tools -b master-kernel-build-2021 prebuilts/kernel-build-tools
     git clone --depth=1 https://android.googlesource.com/platform/prebuilts/build-tools -b master-kernel-build-2021 prebuilts/build-tools
-    fi
-    if [ ! -d "clang" ]; then
+    elif [ ! -d "clang" ]; then
     echo ===========================
     echo Downloading Clang toolchain
+    #source: https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+/refs/heads/android11-qpr2-release/clang-r383902b1/
     if [ $ISACTIONS = 1 ]; then
-    curl -s -o clang/clang.zip --create-dirs ${CLANGDL}
-    else curl -o clang/clang.zip --create-dirs ${CLANGDL}
+    curl -s -o clang/clang.zip --create-dirs https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr2-release/clang-r383902b1.tar.gz
+    else curl -o clang/clang.zip --create-dirs https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android11-qpr2-release/clang-r383902b1.tar.gz
     fi
     cd clang
     tar -xzf clang.zip
     cd ..
-    fi
-    if [ ! -d "gcc" ]; then
+    elif [ ! -d "gcc" ]; then
     echo ====================
     echo Extracting assembler
     #source: https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
@@ -68,86 +48,40 @@ gettools () {
     #echo Installing additional dependencies
     #sudo apt update > /dev/null 2>&1
     #sudo apt install -y rsync
-    set +x
 }
 startbuild () {
-    set -x
     echo Copying configs
     cp build.config.veux common/
-    cp $DEFCONFIG common/arch/arm64/configs/
-    if [ $KSU = 1 ]; then
-        if [[ ! -d common/drivers/kernelsu ]]; then
+    cp qgki_defconfig common/arch/arm64/configs/
+    cp common/arch/arm64/configs/vendor/veux_QGKI.config common/arch/arm64/configs/perf_defconfig
+    if [[ "$1" == *"-ksu"* ]] || [[ "$2" == *"-ksu"* ]] || [ $KSU = 1 ]; then
         echo Integrating KernelSU
         curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-        else
-        echo "KernelSU drivers already added to source, skipping setup script."
-        fi
     fi
-    set +x
-    echo ================================================
     echo Build started on $HOSTNAME with $(nproc) threads
     echo Target:
-    VSUFFIX="$(grep -m 1 "VERSION" common/Makefile | sed 's/.*= *//' | tr -d ' ').$(grep -m 1 "PATCHLEVEL" common/Makefile | sed 's/.*= *//' | tr -d ' ').$(grep -m 1 "SUBLEVEL" common/Makefile | sed 's/.*= *//' | tr -d ' ')"
-    if [ $KSU = 1 ]; then VSUFFIX+="-KernelSU" ; fi
-    echo "Android ${VSUFFIX} (commit $(cd common && git rev-parse HEAD))"
-    echo $VSUFFIX > VERSION.txt
-    echo ================================================
+    echo Android $(grep -m 1 "VERSION" common/Makefile | sed 's/.*= *//' | tr -d ' ').$(grep -m 1 "PATCHLEVEL" common/Makefile | sed 's/.*= *//' | tr -d ' ').$(grep -m 1 "SUBLEVEL" common/Makefile | sed 's/.*= *//' | tr -d ' ') '(commit' $(cd common && git rev-parse HEAD | cut -c 1-8)')'
     echo Calling back-end script...
-    set -x
     if [ $ISACTIONS = 1 ]; then
-        echo "INFO: GitHub Actions host detected, build log won't be piped/redirected"
-        echo "INFO: To retrieve logs, click the gear button next to "Search logs" then "Download log archive""
-        DEFCONFIG="$DEFCONFIG" BUILD_CONFIG=common/build.config.veux build/build.sh
+        echo "GitHub Actions host detected, build log won't piped/redirected"
+        #since Action's console is already a piped output, and Clang won't handle two pipes properly
+        echo "To retrieve logs, click the gear button next to "Search logs" then "Download log archive""
+        BUILD_CONFIG=common/build.config.veux build/build.sh
     elif grep -q "V=1" common/build.config.veux; then
         #Prevent console flooding in verbose mode
-        DEFCONFIG="$DEFCONFIG" BUILD_CONFIG=common/build.config.veux build/build.sh > build.log 2> >(tee -a build.log >&2)
+        BUILD_CONFIG=common/build.config.veux build/build.sh > build.log 2>&1
     else
-        DEFCONFIG="$DEFCONFIG" BUILD_CONFIG=common/build.config.veux build/build.sh 2>&1 | tee build.log
-    fi
-    set +x
-}
-envcheck () {
-    if [[ "$DEFCONFIG" == "ndef" ]]; then
-    echo "ERROR: You didn't complete first-time setup"
-    echo "Open the build.sh file and edit first lines"
-    exit 2
-    else
-        echo DEFCONFIG is $DEFCONFIG
-        echo Kernel source is set to $KERNEL_SOURCE
-        if [[ $KSU == 1 ]]; then
-            echo "KernelSU is enabled for this build"
-            else
-            echo "KernelSU is not enabled. If you have integrated KernelSU before, you might want to redownload source before building."
-        fi
-        echo .
-        if [ $ISACTIONS != 1 ]; then
-        read -p "Are these settings correct? [Y/n] " answer
-        case ${answer:0:1} in
-            y|Y )
-            ;;
-            * )
-            echo "Go back and edit build.sh to your choice"
-            exit 1
-            ;;
-        esac
-        fi
+        BUILD_CONFIG=common/build.config.veux build/build.sh 2>&1 | tee build.log
     fi
 }
 finalize () {
     if [ -e "out/android11-5.4/dist/Image" ]; then
         cp out/android11-5.4/dist/Image AnyKernel3
-        sed -i "s/unknownversion/$(cat VERSION.txt)/g" AnyKernel3/anykernel.sh
-        if [ $INMODULES = 1 ]; then
-            if [ $KSU = 1 ]; then sed -i 's/do.systemless=0/do.systemless=1/g' AnyKernel3/anykernel.sh ; fi
-            cp out/android11-5.4/dist/*.ko AnyKernel3/modules/system/lib/modules
-            cp out/android11-5.4/dist/modules.* AnyKernel3/modules/system/lib/modules
-        fi       
         if [ $ISACTIONS = 1 ]; then echo Workflow will pack up zip file as artifact.
         else
-            echo =========================
             echo Packing to updater zip...
             cd AnyKernel3
-            zip -r5 AnyKernel3_veux-${VSUFFIX}_$(date +'%Y%m%d-%H%M').zip .
+            zip -r5 AnyKernel3_veux_$(date +%Y%m%d).zip .
             mv *.zip .. && cd ..
         fi
     else
@@ -157,10 +91,9 @@ finalize () {
 }
 if [ -n "$1" ]; then
     case "$1" in
-        "envcheck") envcheck ;;
-        "getsource") envcheck && getsource ;;
+        "getsource") getsource ;;
         "gettools") gettools ;;
-        "startbuild") envcheck && startbuild ;;
+        "startbuild") startbuild ;;
         "finalize") finalize ;;
         "debug-cleanup")
             rm -rf common/ build/ prebuilts/ gcc/ out/ clang/ KernelSU/
@@ -168,11 +101,13 @@ if [ -n "$1" ]; then
             rm build.log
             ;;
         *)
-            echo "Unknown option '$1'"
+            echo "Error: Invalid argument '$1'"
             exit 1
             ;;
     esac
 else
-    envcheck && getsource && gettools && startbuild && finalize
+    getsource
+    gettools
+    startbuild
+    finalize
 fi
-
