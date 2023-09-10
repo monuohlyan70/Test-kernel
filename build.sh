369daa7bf18d4f04839b3420bad18fc187efef3b
TanpaNama <nztmscreempendi@yahoo.co.id>
2023-09-10 22:44:59 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ esac
 getsource () {
     if [ ! -d "common" ]; then
     echo Downloading kernel source...
-    git clone --depth=1 https://github.com/RedEnemy30/kernel_xiaomi_sm6375 -b fp common
+    git clone --depth=1 https://github.com/RedEnemy30/kernel_xiaomi_sm6375 -b veuxx common
     fi
 }
 gettools () {
